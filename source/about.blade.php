b2d0cb7c127408ca139f85d0032167ab1ee4e983
Jayson Browne <jaysonb@digitalbridge.com.au>
2021-05-29 22:20:51 +1000

    copy updates

--- source/about.blade.php
+++ source/about.blade.php
@@ -10,14 +10,9 @@
 				<hr>
 			</div>
 			<div class="about__content">
-				<p>Hey there!</p>
-				<p>I'm a user interface designer and developer who has been pushing pixels for the past 3 years.</p>
-				<p>I’m a dedicated, adaptable self-starter - continuously working to hone my design skill-set.</p>
-				<p>I respect the importance of critical feedback, and am constantly striving to reach the best experience for the customer by methodical planning and validating assumptions based on user data.</p>
-				<p>I have a clear, articulate manner and an easy-going, professional personality.</p>
-				<p>When I'm not logged on, you'll probably find me outside in the garden, cooking up a feed or off on a mission somewhere around the globe (once this COVID bizzo chills out a bit).</p>
+				<p>I'm a user interface designer and developer.</p>
 				<p>Give me a shout at <a href="mailto:hi@jaysbrowne.com">hi@jaysbrowne.com</a>.</p>
-				<a href="/"><h3>Back</h3></a>
+				<a href="/"><h3>Home</h3></a>
 			</div>
 		</div>
 		<div class="about__img">
